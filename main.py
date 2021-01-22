aaf43ed75add2bb1f52c6774c7c5a73a2d207ba9
LucasCross2023 <817750@mystma.org>
2021-01-22 16:49:36 +0000

    factORsum try 1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def factORsum(x, word):
+    if word == "factorial": 
+        total = 1
+        for i in range (1, x+1):
+            total = total * i
+          
+    else: 
+        total = 0
+        for i in range (1, x+1):
+            total = total + i
+    
+    return total 
+
+print(factORsum(4, "factorial"))
